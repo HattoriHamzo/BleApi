3e5684935de67dabf0fa4864ea84ae2b36cb7a5b
HattoriHamzo <77831591+HattoriHamzo@users.noreply.github.com>
2022-09-14 13:06:46 +0200

    Delete apiCall directory

--- apiCall/apiCall.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def getAll(endpointName:str):
-    return requests.get(f"https://localhost:7119/api/ble/{endpointName}", verify=False)
-
-def searchByName(endpointName:str, searchName:str):
-    return requests.get(f"https://localhost:7119/api/ble/{endpointName}/{searchName}", verify=False)
