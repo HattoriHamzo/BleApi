c0a25c3bba5d9a0868c5fdf4e13eba3ce220e474
HattoriHamzo <77831591+HattoriHamzo@users.noreply.github.com>
2022-09-19 10:57:02 +0200

    Update program.cs

--- BleApi.Orders/program.cs
+++ BleApi.Orders/program.cs
@@ -1,1 +1,35 @@
+using BleApi.Orders.Interfaces;
+using BleApi.Orders.Service;
+using BleApi.Orders.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("OrdersDB");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddDbContext<OrdersDbContext>(options => {
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
