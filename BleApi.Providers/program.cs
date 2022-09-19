959ebdcde8564421a76dd60070303afd45ce7ffe
HattoriHamzo <77831591+HattoriHamzo@users.noreply.github.com>
2022-09-19 10:57:52 +0200

    Update program.cs

--- BleApi.Providers/program.cs
+++ BleApi.Providers/program.cs
@@ -1,1 +1,35 @@
+using BleApi.Providers.Interfaces;
+using BleApi.Providers.Service;
+using BleApi.Providers.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ProvidersDB");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProvidersService, ProvidersService>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddDbContext<ProvidersDbContext>(options => {
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
